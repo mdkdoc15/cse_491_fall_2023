2b042c3496f255e3091e9fd7b71c916c76cdab41
Endersendd <94008317+Endersendd@users.noreply.github.com>
2023-12-04 13:34:51 -0500

    Updated Documentation for Agent files.

--- source/Agents/AgentFactory.hpp
+++ source/Agents/AgentFactory.hpp
@@ -1,6 +1,9 @@
-//
-// Created by Matthew Kight on 9/24/23.
-//
+/**
+ * This file is part of the Fall 2023, CSE 491 course project.
+ * @brief A class for creating an Agent Factory
+ * @note Status: PROPOSAL
+ * @author Matthew Kight
+ **/
 
 #pragma once
 
@@ -13,120 +16,136 @@
 #include "../core/WorldBase.hpp"
 namespace walle {
 
-struct BaseAgentData {
-    std::string name;
-    cse491::GridPosition position;
-    char symbol = '*';
-};
+    /// @struct BaseAgentData
+    /// @brief Stores basic agent data, i.e. name and position
+    struct BaseAgentData {
+        std::string name;
+        cse491::GridPosition position;
+        char symbol = '*';
+    };
 
-struct AStarAgentData : public BaseAgentData {
-    int recalculate_after_x_turns = 5;
-    cse491::GridPosition target;
-};
+    /// @struct AStarAgentData
+    /// @brief Stores AStar agent data
+    struct AStarAgentData : public BaseAgentData {
+        int recalculate_after_x_turns = 5;
+        cse491::GridPosition target;
+    };
 
-struct PacingAgentData : public BaseAgentData {
-    bool vertical = false;
-};
+    /// @struct PacingAgentData
+    /// @brief Stores Pacing agent data
+    struct PacingAgentData : public BaseAgentData {
+        bool vertical = false;
+    };
 
-struct TrackingAgentData : public BaseAgentData {
-    std::string path;
-    std::vector<cse491::GridPosition> vector_path;
-    cse491::Entity *target;
-    int tracking_distance = 5;
-    cse491::GridPosition start_pos;
-};
+    /// @struct TrackingAgentData
+    /// @brief Stores Tracking agent data
+    struct TrackingAgentData : public BaseAgentData {
+        std::string path;
+        std::vector<cse491::GridPosition> vector_path;
+        cse491::Entity *target;
+        int tracking_distance = 5;
+        cse491::GridPosition start_pos;
+    };
 
-struct PathAgentData : public BaseAgentData {
-    std::string path;
-    std::vector<cse491::GridPosition> vector_path;
-};
+    /// @struct PathAgentData
+    /// @brief Stores Path agent data
+    struct PathAgentData : public BaseAgentData {
+        std::string path;
+        std::vector<cse491::GridPosition> vector_path;
+    };
 
-class AgentFactory {
- private:
-    cse491::WorldBase &world;
- public:
-    AgentFactory() = delete;
-    /**
-     * Constructor for AgentFactory
-     * @param world we are adding agents too
-     */
-    AgentFactory(cse491::WorldBase &world) : world(world) {}
+    /// @class AgentFactory
+    /// @brief A class for creating Agents via the Agent Factory
+    class AgentFactory {
+    private:
+        cse491::WorldBase &world; /// The world the agents belong to
 
-    /**
-     * Add Agent to the world
-     * @param agent_data data for agent we want to create
-     * @return self
-     */
-    AStarAgent &AddAStarAgent(const AStarAgentData &agent_data) {
-        auto &entity = world.AddAgent<walle::AStarAgent>(agent_data.name).SetPosition(agent_data.position).SetProperty(
+    public:
+        AgentFactory() = delete;
+
+        /**
+         * @brief Constructor for AgentFactory
+         * @param world we are adding agents too
+         */
+        AgentFactory(cse491::WorldBase &world) : world(world) {}
+
+        /**
+         * @brief Add Agent to the world
+         * @param agent_data data for agent we want to create
+         * @return self
+         */
+        AStarAgent &AddAStarAgent(const AStarAgentData &agent_data) {
+            auto &entity = world.AddAgent<walle::AStarAgent>(agent_data.name).SetPosition(agent_data.position).SetProperty(
             "symbol",
             agent_data.symbol);
-        assert(dynamic_cast<walle::AStarAgent *>(&entity));
-        auto &agent = static_cast<walle::AStarAgent &>(entity);
-        agent.SetGoalPosition(agent_data.target);
-        agent.SetRecalculate(agent_data.recalculate_after_x_turns);
+            assert(dynamic_cast<walle::AStarAgent *>(&entity));
+            auto &agent = static_cast<walle::AStarAgent &>(entity);
+            agent.SetGoalPosition(agent_data.target);
+            agent.SetRecalculate(agent_data.recalculate_after_x_turns);
 
-        return agent;
-    }
-    /**
-     * Add Agent to the world
-     * @param agent_data data for agent we want to create
-     * @return self
-     */
-    cse491::PacingAgent &AddPacingAgent(const PacingAgentData &agent_data) {
-        auto
+            return agent;
+        }
+
+        /**
+         * @brief Add Agent to the world
+         * @param agent_data data for agent we want to create
+         * @return self
+         */
+        cse491::PacingAgent &AddPacingAgent(const PacingAgentData &agent_data) {
+            auto
             &entity = world.AddAgent<cse491::PacingAgent>(agent_data.name).SetPosition(agent_data.position).SetProperty(
             "symbol",
             agent_data.symbol);
-        assert(dynamic_cast<cse491::PacingAgent *>(&entity));
-        auto &agent = static_cast<cse491::PacingAgent &>(entity);
-        agent.SetVertical(agent_data.vertical);
-        return agent;
-    }
-    /**
-   * Add Agent to the world
-   * @param agent_data data for agent we want to create
-   * @return self
-   */
-    TrackingAgent &AddTrackingAgent(const TrackingAgentData &agent_data) {
-        auto &entity =
+            assert(dynamic_cast<cse491::PacingAgent *>(&entity));
+            auto &agent = static_cast<cse491::PacingAgent &>(entity);
+            agent.SetVertical(agent_data.vertical);
+            return agent;
+        }
+
+        /**
+         * @brief Add Agent to the world
+         * @param agent_data data for agent we want to create
+         * @return self
+         */
+        TrackingAgent &AddTrackingAgent(const TrackingAgentData &agent_data) {
+            auto &entity =
             world.AddAgent<walle::TrackingAgent>(agent_data.name).SetPosition(agent_data.position).SetProperty(
                 "symbol",
                 agent_data.symbol);
-        assert(dynamic_cast<walle::TrackingAgent *>(&entity));
-        auto &agent = static_cast<walle::TrackingAgent &>(entity);
-        if (!agent_data.path.empty())
+            assert(dynamic_cast<walle::TrackingAgent *>(&entity));
+            auto &agent = static_cast<walle::TrackingAgent &>(entity);
+            if (!agent_data.path.empty())
             agent.SetProperty<std::basic_string_view<char>>("path",
-                                                            agent_data.path);
-        else
-            agent.SetPath(agent_data.vector_path);
-        agent.SetTarget(agent_data.target);
-        agent.SetTrackingDistance(agent_data.tracking_distance);
-        agent.SetStartPosition(agent_data.start_pos);
-        agent.Initialize();
-        return agent;
-    }
+                                                                agent_data.path);
+            else
+                agent.SetPath(agent_data.vector_path);
+            agent.SetTarget(agent_data.target);
+            agent.SetTrackingDistance(agent_data.tracking_distance);
+            agent.SetStartPosition(agent_data.start_pos);
+            agent.Initialize();
+            return agent;
+        }
 
-    /**
-   * Add Agent to the world
-   * @param agent_data data for agent we want to create
-   * @return self
-   */
-    PathAgent &AddPathAgent(const PathAgentData &agent_data) {
-        auto &entity =
-            world.AddAgent<walle::PathAgent>(agent_data.name).SetPosition(agent_data.position).SetProperty(
-                "symbol",
-                agent_data.symbol);
-        assert(dynamic_cast<walle::PathAgent *>(&entity));
-        auto &agent = static_cast<walle::PathAgent &>(entity);
-        if (!agent_data.path.empty())
-            agent.SetProperty<std::basic_string_view<char>>("path",
-                                                            agent_data.path); // TODO add another option to provide grid point
-        else
-            agent.SetPath(agent_data.vector_path);
-        agent.Initialize();
-        return agent;
-    }
-};
+        /**
+         * @brief Add Agent to the world
+         * @param agent_data data for agent we want to create
+         * @return self
+         */
+        PathAgent &AddPathAgent(const PathAgentData &agent_data) {
+            auto &entity =
+                world.AddAgent<walle::PathAgent>(agent_data.name).SetPosition(agent_data.position).SetProperty(
+                    "symbol",
+                    agent_data.symbol);
+            assert(dynamic_cast<walle::PathAgent *>(&entity));
+            auto &agent = static_cast<walle::PathAgent &>(entity);
+            if (!agent_data.path.empty())
+                agent.SetProperty<std::basic_string_view<char>>("path",
+                                                                agent_data.path); // TODO add another option to provide grid point
+            else
+                agent.SetPath(agent_data.vector_path);
+            agent.Initialize();
+            return agent;
+        }
+    };
 }
 
--- source/Agents/RandomAgent.hpp
+++ source/Agents/RandomAgent.hpp
@@ -1,5 +1,5 @@
 /**
- * This file is part of the Fall 2023, CSE 491 Course Project - Group 1 Fork - yousif_m Branch.
+ * This file is part of the Fall 2023, CSE 491 Course Project
  * @brief An Agent that will move around using random actions
  * @note Status: PROPOSAL
  * @author Yousif Murrani
@@ -14,66 +14,73 @@
 
 namespace walle {
 
-/**
- * Class for the Random Agent
- */
-class RandomAgent : public cse491::AgentBase {
+    /**
+     * @class RandomAgent
+     * @brief Class for the Random Agent
+     */
+    class RandomAgent : public cse491::AgentBase {
 
-	private:
+        private:
 
-        double mRandom = 0.0;
+            double mRandom = 0.0;   /// Random factor
 
-        bool mMoving = true;
+            bool mMoving = true;    /// Is the agent moving
 
-	public:
-		RandomAgent(size_t id, const std::string &name) : AgentBase(id, name) {
-		}
+        public:
+            /// @brief  Constructor
+            /// @param id ID of the agent
+            /// @param name name of the agent
+            RandomAgent(size_t id, const std::string &name) : AgentBase(id, name) {}
 
-		~RandomAgent() = default;
+            ~RandomAgent() = default;
 
-		/// @brief This agent needs a specific set of actions to function.
-		/// @return Success.
-		bool Initialize() override {
-			return HasAction("up") && HasAction("down") && HasAction("left") && HasAction("right");
-		}
-
-        /// Choose the action to take a step in the random direction
-        size_t SelectAction(const cse491::WorldGrid & /*grid*/,
-                            const cse491::type_options_t & /* type_options*/,
-                            const cse491::item_map_t & /* item_map*/,
-                            const cse491::agent_map_t & /* agent_map*/) override {
-            // We are taking an action so another turn has passed
-
-            CalculateRandom(4);
-
-            if(mMoving){
-                if(mRandom < 1.0){
-                    return action_map["up"];
-                }
-                else if(mRandom < 2.0){
-                    return action_map["down"];
-                }
-                else if(mRandom < 3.0){
-                    return action_map["left"];
-                }
-                else{
-                    return action_map["right"];
-                }
+            /// @brief This agent needs a specific set of actions to function.
+            /// @return Success.
+            bool Initialize() override {
+                return HasAction("up") && HasAction("down") && HasAction("left") && HasAction("right");
             }
 
-            return 0; // should not reach this point
-        }
+            /// @brief Choose the action to take a step in the random direction
+            size_t SelectAction(const cse491::WorldGrid & /*grid*/,
+                                const cse491::type_options_t & /* type_options*/,
+                                const cse491::item_map_t & /* item_map*/,
+                                const cse491::agent_map_t & /* agent_map*/) override {
+                // We are taking an action so another turn has passed
 
-        /// Function to calculate the random direction
-        void CalculateRandom(double multiplier){
-            mRandom = GetWorld().GetRandom(multiplier);
-        }
+                CalculateRandom(4);
 
-        /// Function to set the double for the random variable
-        void SetDirection(double direction) { mRandom = direction; }
+                if(mMoving){
+                    if(mRandom < 1.0){
+                        return action_map["up"];
+                    }
+                    else if(mRandom < 2.0){
+                        return action_map["down"];
+                    }
+                    else if(mRandom < 3.0){
+                        return action_map["left"];
+                    }
+                    else{
+                        return action_map["right"];
+                    }
+                }
 
-        void SetMoving(bool move) { mMoving = move; }
+                return 0; // should not reach this point
+            }
+           
+            /// @brief Function to calculate the random direction
+            /// @param multiplier random multiplier
+            void CalculateRandom(double multiplier){
+                mRandom = GetWorld().GetRandom(multiplier);
+            }
 
-};
+            /// @brief Function to set the double for the random variable
+            /// @param direction direction to set
+            void SetDirection(double direction) { mRandom = direction; }
 
-}
+            /// @brief Function to set the bool for the move variable
+            /// @param move move to set
+            void SetMoving(bool move) { mMoving = move; }
+
+    };
+
+} // namespace walle
